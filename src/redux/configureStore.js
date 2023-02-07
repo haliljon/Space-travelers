f4475c86883ecf0723f7c6fbc00558d1b3059546
haliljon <110017001+haliljon@users.noreply.github.com>
2023-02-08 00:59:05 +0500

    Added mission joining
    Co-Authored-By: George M'sapenda <georgecoopmsapenda@gmail.com>

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './missions/missions';
 import { rocketsReducer } from './rockets/rockets';
 
-// const rootReducer = combineReducers({ books: booksReducer, status: checkStatusReducer });
 const store = configureStore({ reducer: { rockets: rocketsReducer, missions: missionsReducer } });
 
 export default store;
